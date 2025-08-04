00d7037860ddedb841dcaff772b5b66839d0bd1f
seungin <tmddls91@inha.edu>
2025-08-04 16:20:44 +0900

    [chore] 테스트 폴더 구조 생성

---
+++ src/test/java/com/ll/article/ArticleRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.ll.article;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArticleRepositoryTest {
+    @Test
+    @DisplayName("게시글 저장 테스트 (예시입니다.")
+    void t1() {
+        // TODO: ArticleRepository 관련 TDD 테스트 작성
+    }
+}
---
+++ src/test/java/com/ll/article/ArticleServiceTest.java
@@ -0,0 +1,12 @@
+package com.ll.article;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArticleServiceTest {
+    @Test
+    @DisplayName("게시글 작성 테스트 (예시입니다.")
+    void t1() {
+        // TODO: ArticleService 관련 TDD 테스트 작성
+    }
+}
