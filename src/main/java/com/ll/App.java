e8bb7cb4ad1c02a849e6b3bdcf1c34ff6ea93711
seungin <tmddls91@inha.edu>
2025-08-05 21:46:48 +0900

    [feat] App에 exit 종료 조건 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,5 +6,15 @@ public class App {
     public void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 게시판 앱 만들기 ==");
+
+        while (true) {
+            System.out.print("명령어: ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
     }
 }
---
+++ src/test/java/com/ll/article/AppTest.java
@@ -0,0 +1,4 @@
+package com.ll.article;
+
+public class AppTest {
+}
