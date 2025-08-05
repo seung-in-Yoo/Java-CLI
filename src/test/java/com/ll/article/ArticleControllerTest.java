4f0218498270c58206f90b465301c34c30928217
jooky <jooky0327@naver.com>
2025-08-06 01:11:41 +0900

    [Test] List기능 테스트 작성

--- src/test/java/com/ll/article/ArticleControllerTest.java
+++ src/test/java/com/ll/article/ArticleControllerTest.java
@@ -19,4 +19,24 @@ public class ArticleControllerTest {
 
         assertThat(rs).contains("1번 게시글이 등록되었습니다.");
     }
+
+    @Test
+    @DisplayName("list")
+    public void t2() {
+        String rs = AppTestRunner.run("""
+                write
+                자바 공부
+                자바 텍스트 게시판 만들기
+                write
+                spring 공부
+                spring 게시판 만들기
+                list
+                """);
+
+        assertThat(rs).contains("1번 게시글이 등록되었습니다.")
+                .contains("2번 게시글이 등록되었습니다.")
+                .contains(" 번호 | 제목 | 등록일")
+                .contains(" 1 | 자바 공부 | 2025-08-06")
+                .contains(" 2 | spring 공부 | 2025-08-06");
+    }
 }
