5566ec688dd8f5716d566645c2f9898112148dd9
seungin <tmddls91@inha.edu>
2025-08-05 22:12:31 +0900

    [feat] 명령어 파싱을 위한 Rq 유틸리티 구현

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,5 +1,32 @@
 package com.ll;
 
 public class Rq {
-    // TODO: 사용자 입력 유틸 구현
+    private final String actionName;
+    private final int paramId;
+
+    public Rq(String command) {
+        String[] split = command.split(" ");
+
+        this.actionName = split[0];
+
+        int id = -1;
+
+        // 두 번째 단어가 있다면 파싱해서 저장하고 없으면 -1 처리
+        if (split.length > 1) {
+            try {
+                id = Integer.parseInt(split[1]);
+            } catch (NumberFormatException e) {
+                // 숫자로 바꿀 수 없으면 NumberFormatExeption 예외 처리
+            }
+        }
+        this.paramId = id;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public int getParamId() {
+        return paramId;
+    }
 }
