ebc08ba2b06e414dfed80a58c88bd970af727620
jiyoon-00 <95743677+jiyoon35@users.noreply.github.com>
2025-08-07 01:38:53 +0900

    [feat] 게시글 리스트 2개 역순 출력 테스트 (레드)

--- src/test/java/com/ll/article/AppTest.java
+++ src/test/java/com/ll/article/AppTest.java
@@ -3,6 +3,9 @@ package com.ll.article;
 import com.ll.App;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -21,5 +24,22 @@ public class AppTest {
         assertEquals("내용1", article.getContent());
         assertEquals("2025-08-06", article.getRegDate());
         assertEquals(1, article.getId());
+    }
+
+    @Test
+    void t2() {
+        App app = new App();
+        app.write("제목1", "내용1", "2025-08-06");
+        app.write("제목2", "내용2", "2025-08-07");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        app.list();
+
+        String result = out.toString().trim();
+
+        assertTrue(result.contains("2 / 제목2 / 2025-08-07"));
+        assertTrue(result.contains("1 / 제목1 / 2025-08-06"));
     }
 }
